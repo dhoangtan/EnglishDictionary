cc38cf18244bb175acfbb9a975169b47a388260e
danhhoangtan <danhhoangtan.it@outlook.com>
2023-03-19 19:35:56 +0700

    feature: load user profile

--- app/src/main/java/android/englishdictionary/fragments/ProfileFragment.java
+++ app/src/main/java/android/englishdictionary/fragments/ProfileFragment.java
@@ -3,11 +3,25 @@ package android.englishdictionary.fragments;
 import android.englishdictionary.R;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,6 +34,9 @@ public class ProfileFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private ImageView userAvatarImageView;
+    private TextView userFullNameTextView, userEmailTextView;
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -61,5 +78,37 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        userAvatarImageView = view.findViewById(R.id.fr_profile_user_avatar_image_view);
+        userFullNameTextView = view.findViewById(R.id.fr_profile_user_full_name_text_view);
+        userEmailTextView = view.findViewById(R.id.fr_profile_user_email_text_view);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        DocumentReference documentReference = FirebaseFirestore
+                .getInstance()
+                .collection("users")
+                .document(user.getUid());
+
+        documentReference.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String,Object> data = document.getData();
+                        String email = data.get("email").toString();
+                        String fullName = data.get("full_name").toString();
+
+                        userFullNameTextView.setText(fullName);
+                        userEmailTextView.setText(email);
+                    }
+                }
+            }
+        });
+
     }
 }
