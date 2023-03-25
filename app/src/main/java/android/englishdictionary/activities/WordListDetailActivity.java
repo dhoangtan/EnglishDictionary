ec9defc17e055eb2b524dec96d9edc5a2ba07d88
danhhoangtan <danhhoangtan.it@outlook.com>
2023-03-25 15:30:26 +0700

    feature: sort feature

--- app/src/main/java/android/englishdictionary/activities/WordListDetailActivity.java
+++ app/src/main/java/android/englishdictionary/activities/WordListDetailActivity.java
@@ -3,6 +3,7 @@ package android.englishdictionary.activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.englishdictionary.R;
 import android.englishdictionary.adapters.WordListDetailAdapter;
@@ -12,6 +13,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class WordListDetailActivity extends AppCompatActivity {
@@ -19,6 +21,7 @@ public class WordListDetailActivity extends AppCompatActivity {
     private RecyclerView wordsRecyclerView;
     private Button sortButton, modifyButton;
 
+    @SuppressLint("NotifyDataSetChanged")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +31,17 @@ public class WordListDetailActivity extends AppCompatActivity {
         WordList wordList = (WordList) intent.getExtras().get("word_list");
 
         wordListNameTextView = findViewById(R.id.ac_wordlist_detail_wordlist_name_text_view);
-
         wordsRecyclerView = findViewById(R.id.ac_wordlist_detail_words_recycler_view);
+        sortButton = findViewById(R.id.ac_wordlist_detail_sort_button);
+
+        WordListDetailAdapter detailAdapter = new WordListDetailAdapter(getApplicationContext(), (ArrayList<WordList.WordListData>) wordList.getWords());
 
         wordListNameTextView.setText(wordList.getName());
-        wordsRecyclerView.setAdapter(new WordListDetailAdapter(getApplicationContext(), (ArrayList<WordList.WordListData>) wordList.getWords()));
+        wordsRecyclerView.setAdapter(detailAdapter);
+
+        sortButton.setOnClickListener(view -> {
+            wordList.getWords().sort((w1, w2) -> w1.getWord().compareTo(w2.getWord()));
+            detailAdapter.notifyDataSetChanged();
+        });
     }
 }
